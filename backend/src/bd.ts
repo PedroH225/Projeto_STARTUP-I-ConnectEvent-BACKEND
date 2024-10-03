5df03c539e463475e422c6f24c6786a47e2e3de2
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2024-10-03 14:16:22 -0300

    Conexão com o banco de dados

---
+++ backend/src/bd.ts
@@ -0,0 +1,21 @@
+import { DataSource } from "typeorm"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "password",
+    database: "connect-event-db",
+    entities: ["src/entities/*.ts"],
+
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Banco de dados iniciado!');
+  })
+  .catch((error) => {
+    console.error('Erro ao iniciar o banco de dados:', error);
+  });
+
