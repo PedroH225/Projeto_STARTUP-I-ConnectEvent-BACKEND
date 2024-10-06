534543982e070d597d790856ca9c71cd8126d5f6
VictorAlexandreMuller <victor.alexandre.mu@gmail.com>
2024-10-05 22:41:29 -0300

    inicio do navBar e footer

--- frontend/connectEvent/src/app/app-routing.module.ts
+++ frontend/connectEvent/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { FilterComponent } from './nav-bar/filter/filter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'footer', component: FooterComponent },
+  {
+    path: 'navBar',
+    component: NavBarComponent,
+    children: [{ path: 'filter', component: FilterComponent }],
+  },
+  { path: 'sideMenu', component: SideMenuComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- frontend/connectEvent/src/app/app.module.ts
+++ frontend/connectEvent/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { FilterComponent } from './nav-bar/filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    SideMenuComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
