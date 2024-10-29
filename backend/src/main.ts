9710be0d57d61a62c5d625306d95b857dd95a5f2
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2024-10-29 20:10:36 -0300

    Registro e Visualização de fotos

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,8 +3,12 @@ import express from "express";
 import "./bd";
 import { routes } from "./rotas";
 import cors from "cors";
+import path from 'path';
 
 const app = express();
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
 
 // Configurar CORS aqui
 app.use(cors()); // Permite requisições de outras origens (como seu frontend Angular)
