e785dbf9eb5d83db67277256e532e8b1287d55e3
Pedro Guimarães <pedrodguimaraes20@gmail.com>
2024-11-06 17:45:06 -0300

    Gráficos pizza gênero e histograma idades

--- back-estatisticas/app.py
+++ back-estatisticas/app.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+
 import matplotlib.pyplot as plt
 from typing import List
 from io import BytesIO
 from fastapi.responses import StreamingResponse
+from collections import Counter
 
 
 app = FastAPI()
-
 # Modelo para o JSON recebido
 class Data(BaseModel):
     values: List[int]  # Lista de inteiros
@@ -24,6 +27,65 @@ def generate_chart(data: Data):
     plt.xlabel("Índice")
     plt.ylabel("Valor")
     plt.title("Gráfico de Barras dos Valores")
+
+    # Salvando o gráfico em um buffer de bytes
+    buffer = BytesIO()
+    plt.savefig(buffer, format="png")
+    buffer.seek(0)
+    plt.close()
+
+    # Retornando o gráfico como uma imagem
+    return StreamingResponse(buffer, media_type="image/png")
+
+
+
+class Usuario(BaseModel):
+    id: int
+    email: str
+    idade: int
+    genero: str
+    estado: str
+    cidade: str
+
+@app.post("/generate-pie-chart")
+def generate_pie_chart(usuarios: list[Usuario]):
+    # Contando a quantidade de cada gênero
+    generos = [usuario.genero for usuario in usuarios]
+    contagem_generos = Counter(generos)
+
+    # Extraindo os dados para o gráfico
+    labels = list(contagem_generos.keys())
+    sizes = list(contagem_generos.values())
+
+    # Criando o gráfico de pizza
+    plt.figure(figsize=(7, 7))
+    plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90, colors=['skyblue', 'lightcoral', 'lightgreen'])
+
+    # Título
+    plt.title('Distribuição de Gênero dos Usuários')
+
+    # Salvando o gráfico em um buffer de bytes
+    buffer = BytesIO()
+    plt.savefig(buffer, format="png")
+    buffer.seek(0)
+    plt.close()
+
+    # Retornando o gráfico como uma imagem
+    return StreamingResponse(buffer, media_type="image/png")
+
+
+# Endpoint para gerar o gráfico de distribuição de idade
+@app.post("/generate-age-distribution")
+def generate_age_distribution(usuarios: list[Usuario]):
+    # Extraindo as idades dos participantes
+    idades = [usuario.idade for usuario in usuarios]
+    
+    # Criando o histograma de idades
+    plt.figure(figsize=(10, 6))
+    plt.hist(idades, bins=range(min(idades), max(idades) + 4, 4), color='skyblue', edgecolor='black', alpha=0.7)
+    plt.xlabel("Faixa Etária")
+    plt.ylabel("Número de Participantes")
+    plt.title("Distribuição de Idade dos Participantes")
 
     # Salvando o gráfico em um buffer de bytes
     buffer = BytesIO()
